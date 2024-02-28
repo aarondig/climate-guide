59955e0dc18690423ce143b784bdfce0cb8b94c0
Aaron Diggdon <aarondiggdon@gmail.com>
2024-02-28 21:11:19 +0000

    final version

--- client/src/apis/probablefutures.js
+++ client/src/apis/probablefutures.js
@@ -1,101 +1,86 @@
-// var request = require("request");
+// const axios = require("axios");
+// const router = require("express").Router();
+import axios from "axios";
 
-// const pfApiUrl = "https://graphql.probablefutures.org/graphql";
-// const pfTokenAudience = "https://graphql.probablefutures.com";
-// const pfTokenUrl = "https://probablefutures.us.auth0.com/oauth/token";
+const pfApiUrl = "https://graphql.probablefutures.org";
+const pfTokenAudience = "https://graphql.probablefutures.com";
+const pfTokenUrl = "https://probablefutures.us.auth0.com/oauth/token";
 
-//  async function getPfData(address, country, warmingScenario = "1.5") {
-//     const location = `
-//         country: "${country}"
-//         address: "${address}"
-//     `;
+const clientId = "FQCzxCCBAh0wih1Yx0DFqheeldF0T6FD"
+const clientSecret = "v6mbHDocvyWPM9pNYNBxvdZda1_yFq4DY_1QS5kVNWuGZGlDg4Lri89szt1KIe8W"
 
-//     const query = `
-//         mutation {
-//             getDatasetStatistics(input: { ${location}
-//                     warmingScenario: "${warmingScenario}"
-//                 }) {
-//                 datasetStatisticsResponses{
-//                     datasetId
-//                     midValue
-//                     name
-//                     unit
-//                     warmingScenario
-//                     latitude
-//                     longitude
-//                     info
-//                 }
-//             }
-//         }
-//     `;
+// router.get("/", (req, res) => {
 
-//     console.log(query);
-
-//     try {
-//         const accessToken = await getPfToken();
-//         const response = await fetch(pfApiUrl + "/graphql", {
-//             method: 'POST',
-//             headers: {
-//                 'Content-Type': 'application/json',
-//                 'Authorization': 'Bearer ' + accessToken,
-//             },
-//             body: JSON.stringify({ query: query, variables: {} }),
-//         });
-//         const data = await response.json();
-//         return JSON.stringify(data);
-//     } catch (error) {
-//         console.error('Error fetching the data:', error);
-//         return null;
-//     }
-// }
-
-const clientId = process.env.REACT_APP_PF_CLIENT_ID;
-const clientSecret = process.env.REACT_APP_PF_PASSWORD;
-
-
-
-async function getPFToken() {
-
-
-
-var options = { method: 'POST',
-url: 'https://probablefutures.us.auth0.com/oauth/token',
-headers: { 'content-type': 'application/json' },
-body: `{"client_id":"${clientId}","client_secret":"${clientSecret}","audience":"https://graphql.probablefutures.com","grant_type":"client_credentials"}` };
-
-fetch(options, function (error, response, body) {
-if (error) throw new Error(error);
-console.log(body);
-
-
-
-});
-
-}
-export async function getPFData() {
-  console.log(process.env.REACT_APP_PF_CLIENT_ID);
-
-
-  var headers = new Headers();
-  headers.append("Authorization", `Bearer ${getPFToken()}`);
-  headers.append("Content-Type", "application/json");
-
-  var graphql = JSON.stringify({
-    query:
-      'mutation {\n      getDatasetStatistics(\n        input: {country: "us", address: "121 East 48th Street, Apt. 123, New York, NY 10001", warmingScenario: "1.5", datasetId: null}\n      ) {\n        datasetStatisticsResponses {\n          datasetId\n          highValue\n          lowValue\n          midValue\n          name\n          unit\n          warmingScenario\n          longitude\n          latitude\n      }\n    }\n}',
-    variables: {},
-  });
-  var requestOptions = {
-    method: "POST",
-    headers: headers,
-    body: graphql,
-  };
-
-  fetch("https://graphql.probablefutures.org/graphql", requestOptions)
-    .then((response) => response.text())
-    .then((result) => console.log(result))
-    .catch((error) => console.log("error", error));
-
-
+  async function getPfToken() {
+    // const clientId = process.env.REACT_APP_PF_CLIENT_ID;
+    // const clientSecret = process.env.REACT_APP_PF_PASSWORD;
+    try {
+        const response = await axios.post(pfTokenUrl, {
+            client_id: clientId,
+            client_secret: clientSecret,
+            audience: pfTokenAudience,
+            grant_type: "client_credentials",
+        });
+        const accessToken = response.data.access_token;
+        return accessToken;
+    } catch (error) {
+        console.error('Error fetching the access token:', error);
+        return null;
+    }
 }
 
+async function getPfData() {
+  // console.log(country)
+  // console.log(address)
+  const country = "United Kingdom";
+  const address = "London";
+  const warmingScenario = 1.5;
+
+
+// const country = req.query.country;
+// const address = req.query.address;
+// const warmingScenario = `${req.query.warming_scenario !== undefined ? req.query.warming_scenario : 1.5}`;
+
+    const variables = {};
+    const location = `
+        country: "${country}"
+        address: "${address}"
+    `;
+
+    const query = `
+        mutation {
+            getDatasetStatistics(input: { ${location}
+                    warmingScenario: "${warmingScenario}" 
+                }) {
+                datasetStatisticsResponses{
+                    datasetId
+                    midValue
+                    name
+                    unit
+                    warmingScenario
+                    latitude
+                    longitude
+                    info
+                }
+            }
+        }
+    `;
+
+    try {
+        const accessToken = await getPfToken();
+        const url = pfApiUrl + "/graphql";
+        const headers = { Authorization: "Bearer " + accessToken };
+        const response = await axios.post(url, { query: query, variables: variables }, { headers: headers });
+        // await res.send(JSON.stringify(response.data))
+        return JSON.stringify(response.data);
+    } catch (error) {
+        console.error('Error fetching the data:', error);
+        return null;
+    }
+  }
+
+
+export default getPfData();
+// });
+
+// module.exports = router;
